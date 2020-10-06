ea3c0587fc067a589108445cdb2c5d52c1d9fd58
tarasfrompir <tarasfrompir@inbox.ru>
2020-10-06 12:29:52 +0300

    Update objects.class.php

--- modules/objects/objects.class.php
+++ modules/objects/objects.class.php
@@ -1039,7 +1039,8 @@ class objects extends module
                 $params['OLD_VALUE'] = (string)$old_value;
                 $params['SOURCE'] = (string)$source;
                 //$this->callMethod($prop['ONCHANGE'], $params);
-                $this->callMethodSafe($prop['ONCHANGE'], $params);
+                //$this->callMethodSafe($prop['ONCHANGE'], $params);
+                $this->raiseEvent($prop['ONCHANGE'], $params);
                 unset($property_linked_history[$this->object_title . '.' . $property][$prop['ONCHANGE']]);
             }
         }
